56c5e79c9174a01eb297543f9c8b14555c2bb03b
laurens_kersbergen <lvkersbergen@gmail.com>
2022-02-16 09:02:49 +0100

    Update dark mode/hover colors

--- src/components/sections/Work.tsx
+++ src/components/sections/Work.tsx
@@ -30,107 +30,107 @@ export function Work(props: ComponentProps<"section">) {
         <ul>
           <li className="my-5 flex items-center gap-4">
             <a
-              className="link focusable font-medium text-zinc-800 dark:text-white"
+              className="font-medium text-zinc-800 hover:text-blue-600 hover:underline dark:text-white dark:hover:text-blue-600"
               href="https://www.miro.com/"
               rel="noreferrer"
               target="_blank"
             >
               Miro
             </a>
-            <span className="truncate text-neutral-400">
+            <span className="wave flex-1" />
+            <span className="truncate text-neutral-500">
               Product Manager, Website
             </span>
-            <span className="wave flex-1" />
             <span className="-mr-1 whitespace-nowrap">
-              <span className="inline-block w-10 text-center text-neutral-500">
+              <span className="inline-block w-10 text-center text-neutral-400">
                 2022
               </span>
-              <span className="text-neutral-500 dark:text-zinc-550">—</span>
+              <span className="text-neutral-400 dark:text-zinc-550">—</span>
               <span className="inline-block w-10 text-center" />
             </span>
           </li>
           <li className="my-5 flex items-center gap-4">
             <a
-              className="link focusable font-medium text-zinc-800 dark:text-white"
+              className="font-medium text-zinc-800 hover:text-blue-600 hover:underline dark:text-white dark:hover:text-blue-600"
               href="https://www.growthtribe.io/"
               rel="noreferrer"
               target="_blank"
             >
               Growth Tribe
             </a>
-            <span className="truncate text-neutral-400">Product Manager</span>
             <span className="wave flex-1" />
+            <span className="truncate text-neutral-500">Product Manager</span>
             <span className="-mr-1 whitespace-nowrap">
-              <span className="inline-block w-10 text-center text-neutral-500">
+              <span className="inline-block w-10 text-center text-neutral-400">
                 2021
               </span>
-              <span className="text-neutral-500 dark:text-zinc-550">—</span>
-              <span className="inline-block w-10 text-center text-neutral-500">
+              <span className="text-neutral-400 dark:text-zinc-550">—</span>
+              <span className="inline-block w-10 text-center text-neutral-400">
                 2022
               </span>
             </span>
           </li>
           <li className="my-5 flex items-center gap-4">
             <a
-              className="link focusable font-medium text-zinc-800 dark:text-white"
+              className="font-medium text-zinc-800 hover:text-blue-600 hover:underline dark:text-white dark:hover:text-blue-600"
               href="https://www.kpn.com/"
               rel="noreferrer"
               target="_blank"
             >
               KPN.com
             </a>
-            <span className="truncate text-neutral-400">
+            <span className="wave flex-1" />
+            <span className="truncate text-neutral-500">
               Product Manager, Growth
             </span>
-            <span className="wave flex-1" />
             <span className="-mr-1 whitespace-nowrap">
-              <span className="inline-block w-10 text-center text-neutral-500">
+              <span className="inline-block w-10 text-center text-neutral-400">
                 2019
               </span>
-              <span className="text-neutral-500 dark:text-zinc-550">—</span>
-              <span className="inline-block w-10 text-center text-neutral-500">
+              <span className="text-neutral-400 dark:text-zinc-550">—</span>
+              <span className="inline-block w-10 text-center text-neutral-400">
                 2021
               </span>
             </span>
           </li>
           <li className="my-5 flex items-center gap-4">
             <a
-              className="link focusable font-medium text-zinc-800 dark:text-white"
+              className="font-medium text-zinc-800 hover:text-blue-600 hover:underline dark:text-white dark:hover:text-blue-600"
               href="https://www.kpn.com/"
               rel="noreferrer"
               target="_blank"
             >
               KPN.com
             </a>
-            <span className="truncate text-neutral-400">Marketing Manager</span>
             <span className="wave flex-1" />
+            <span className="truncate text-neutral-500">Marketing Manager</span>
             <span className="-mr-1 whitespace-nowrap">
-              <span className="inline-block w-10 text-center text-neutral-500">
+              <span className="inline-block w-10 text-center text-neutral-400">
                 2018
               </span>
-              <span className=" text-neutral-500 dark:text-zinc-550">—</span>
-              <span className="inline-block w-10 text-center text-neutral-500">
+              <span className=" text-neutral-400 dark:text-zinc-550">—</span>
+              <span className="inline-block w-10 text-center text-neutral-400">
                 2019
               </span>
             </span>
           </li>
           <li className="my-5 flex items-center gap-4">
             <a
-              className="link focusable font-medium text-zinc-800 dark:text-white"
+              className="font-medium text-zinc-800 hover:text-blue-600 hover:underline dark:text-white dark:hover:text-blue-600"
               href="https://www.vinla.nl/"
               rel="noreferrer"
               target="_blank"
             >
               Vinla
             </a>
-            <span className="truncate text-neutral-400">Founder & Owner</span>
             <span className="wave flex-1" />
+            <span className="truncate text-neutral-500">Founder & Owner</span>
             <span className="-mr-1 whitespace-nowrap">
-              <span className="inline-block w-10 text-center text-neutral-500">
+              <span className="inline-block w-10 text-center text-neutral-400">
                 2011
               </span>
               <span className="text-neutral-400 dark:text-zinc-500">—</span>
-              <span className="inline-block w-10 text-center text-neutral-500">
+              <span className="inline-block w-10 text-center text-neutral-400">
                 2018
               </span>
             </span>
